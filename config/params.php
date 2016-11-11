47b588baae93dc3acc96821d1f6604db663d9c07
yottamug <yottamug@gmail.com>
2016-11-11 16:53:48 +0800

    Create params.php

---
+++ config/params.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+	'backendUrl'	=> 'http://example.com/bzcast/web/',
+    'adminEmail'	=> 'admin@example.com',
+	'podcastdir'	=> '/media/audio/radio/',
+	'feeddir'		=> __DIR__.'/../feed/',
+	
+	
+	'podcast_link'			=> 'http://example.com/bzcast/',
+	'podcast_image'			=> 'http://example.com/bzcast/podcast.jpg',
+	
+	
+	'podcast_title'			=> 'Podcast Library',
+	'podcast_subtitle'		=> 'Latest Podcast',
+	'podcast_summary'		=> 'Latest Podcast',
+	'podcast_description'	=> 'Latest Podcast',
+	'podcast_feedname'		=> 'bzfeed',
+	'podcast_url'			=> 'http://example.com/bzcast/',
+	
+	'feed_image_url'		=> 'http://example.com/bzcast/podcast.jpg',
+	'feed_base_url'			=> 'http://example.com/bzcast/feed/',
+	'feed_base_dir'			=> __DIR__.'/../feed/',
+	'feed_explicit'			=> 'no',
+	'feed_author'			=> 'yottamug',
+	'feed_name'				=> 'yottamug',
+	'feed_email'			=> 'admin@example.com',
+	'feed_base_downloadurl'	=> 'http://example.com/bzcast/web/index.php/download/',
+	
+];
