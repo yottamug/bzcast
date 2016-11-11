b3d547612fae8b046fde7ea74016283dd791e792
yottamug <yottamug@gmail.com>
2016-11-11 16:51:04 +0800

    Create db.php

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'sqlite:/var/www/html/bzcast/protected/bzcast.sqlite3',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+];
